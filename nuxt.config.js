96131f5168f85378ab8292b2646e289759005a1e
maekawa-syuki <syu54117123>
2021-09-09 15:37:10 +0900

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,8 +31,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-    '@nuxtjs/pwa',
+    '@nuxtjs/eslint-module'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
